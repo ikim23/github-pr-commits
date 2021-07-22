72ecb961a910e0bc285a324c7fc6d44f7f84284b
ikim23 <ikim23@users.noreply.github.com>
2021-07-22 22:43:48 +0200

    auth

--- src/content.js
+++ src/content.js
@@ -1,18 +1,30 @@
 import { Octokit } from "octokit";
 
+const URL_PATTERN = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/pull\/(\d+)/;
+
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log("DOM is ready.");
+  console.log("DOM is ready!");
 
-  const octokit = new Octokit();
+  // https://github.com/octokit/auth-token.js
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-  const data = await octokit.request(
+  const params = parseUrl();
+  const { data } = await octokit.request(
     "GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
-    {
-      owner: "facebook",
-      repo: "react",
-      pull_number: 1,
-    }
+    params
   );
 
+  const commits = data.map(({ sha, commit }) => ({
+    sha,
+    message: commit.message,
+    date: commit.author.date,
+  }));
+
   console.log(data);
+  console.log(commits);
 });
+
+function parseUrl() {
+  const [, owner, repo, pull_number] = window.location.href.match(URL_PATTERN);
+  return { owner, repo, pull_number };
+}

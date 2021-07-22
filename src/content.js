d086d90135c2c230ff3bf22f282c5f34f1cdb998
ikim23 <ikim23@users.noreply.github.com>
2021-07-22 23:41:51 +0200

    listen on COMMIT_KEY

--- src/content.js
+++ src/content.js
@@ -1,30 +1,56 @@
-import { Octokit } from "octokit";
+import $ from 'jquery'
+import { Octokit } from 'octokit'
 
-const URL_PATTERN = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/pull\/(\d+)/;
+const URL_PATTERN = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/pull\/(\d+)*/
+const COMMIT_KEY = 'cmt'
 
-document.addEventListener("DOMContentLoaded", async () => {
-  console.log("DOM is ready!");
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
 
-  // https://github.com/octokit/auth-token.js
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+$(async () => {
+  let commits = []
 
-  const params = parseUrl();
-  const { data } = await octokit.request(
-    "GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
-    params
-  );
+  $('.comment-form-textarea')
+    .on('focus', async () => {
+      commits = await getPullCommits()
+    })
+    .on('input', (event) => {
+      const { value } = event.target
+
+      if (hasCommitKey(value)) {
+        console.log(commits)
+      }
+    })
+})
+
+async function getPullCommits() {
+  const pullParams = parsePullUrl()
+  if (!pullParams) {
+    return []
+  }
+
+  const { data } = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', pullParams)
+  console.log(data)
 
   const commits = data.map(({ sha, commit }) => ({
     sha,
     message: commit.message,
     date: commit.author.date,
-  }));
+  }))
 
-  console.log(data);
-  console.log(commits);
-});
+  return commits
+}
 
-function parseUrl() {
-  const [, owner, repo, pull_number] = window.location.href.match(URL_PATTERN);
-  return { owner, repo, pull_number };
+function parsePullUrl() {
+  const match = window.location.href.match(URL_PATTERN)
+  if (!match) {
+    return null
+  }
+
+  const [, owner, repo, pull_number] = match
+
+  return { owner, repo, pull_number }
+}
+
+function hasCommitKey(text) {
+  return text == COMMIT_KEY || text.substr(-(COMMIT_KEY.length + 1)) == ' ' + COMMIT_KEY
 }

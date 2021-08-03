bee71c7a4c7f47c48f4e2334fbc1626b41ecf90a
ikim23 <ikim23@users.noreply.github.com>
2021-08-03 16:48:23 +0200

    listen on new commits in timeline

--- src/content/content.js
+++ src/content/content.js
@@ -1,24 +1,54 @@
 import _ from 'lodash'
 import GithubPullCommit from './GithubPullCommits'
-import { addLocationChangeListener, getOptions, addOptionsChangeListener } from '../utils'
+import { getOptions, addOptionsChangeListener } from '../utils'
 
 const COMMENT_FORM_CLASS = 'comment-form-textarea'
 
+/**
+ * Fetch commits on:
+ *  - window focus
+ *  - location change
+ *  - new commit in timeline
+ */
+function addPullRequestPageListener(listener) {
+  let prevHref = null
+  let commitCount = 0
+
+  const observer = new MutationObserver(() => {
+    if (prevHref != document.location.href) {
+      prevHref = document.location.href
+      listener()
+    } else {
+      const currentCommitCount = document.querySelectorAll('.js-commit').length
+      if (commitCount < currentCommitCount) {
+        commitCount = currentCommitCount
+        listener()
+      }
+    }
+  })
+
+  observer.observe(document.querySelector('body'), { childList: true, subtree: true })
+
+  window.addEventListener('focus', listener)
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   let app = null
+  const fetchCommits = _.debounce(
+    () => {
+      app.fetchCommits()
+    },
+    300,
+    { leading: true, trailing: false }
+  )
   const initialize = (options) => {
     app = GithubPullCommit(options)
-    app.fetchCommits()
-  }
-  const fetchCommits = () => {
-    app.fetchCommits()
+    fetchCommits()
   }
 
   getOptions(initialize)
   addOptionsChangeListener(initialize)
-
-  window.addEventListener('focus', fetchCommits)
-  addLocationChangeListener(fetchCommits)
+  addPullRequestPageListener(fetchCommits)
 
   const keyActions = {
     Enter: () => app.enterCommit(),
